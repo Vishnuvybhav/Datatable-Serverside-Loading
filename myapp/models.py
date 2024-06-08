43d0aee0edc7a09aef52c4320d83d72fdf1ed75c
Vasanth V V <vasanthvdev@gmail.com>
2024-06-08 20:14:55 +0530

    added foreign keys compatability

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    manager = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} -- {self.location}'
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -7,6 +14,8 @@ class Employee(models.Model):
     office = models.CharField(max_length=100)
     start_date = models.DateField()
     salary = models.DecimalField(max_digits=10, decimal_places=2)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
